948ae81f37a27fadb7b7c1a233ceeb6df06510a6
Julio Cesar <cesarru96@gmail.com>
2022-03-02 13:44:14 -0600

    Changed the whole code, when detected a face there will be a g

--- camera.py
+++ camera.py
@@ -1,15 +1,42 @@
 import cv2
+import sys
+faceCascade = cv2.CascadeClassifier('C:/Users/Verona/AppData/Local/Programs/Python/Python39/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
 # Check if the webcam is opened correctly
-if not cap.isOpened():
+if not video_capture.isOpened():
     raise IOError("Cannot open webcam")
 
 while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
 
-cap.release()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    n=0
+    for (x, y, w, h) in faces:
+        n += 1
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        cv2.putText(frame, f"Face {n}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX,
+		0.55, (0, 255, 0), 2)
+    
+    # Display the resulting frame
+    cv2.putText(frame, f"Total faces: {n}", (1,25), cv2.FONT_HERSHEY_SIMPLEX,
+		0.50, (255, 80, 0), 2)
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
